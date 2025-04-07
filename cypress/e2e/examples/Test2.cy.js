06c08f14fadb8c84fbb5093bf53199b13ebc9c3b
wienm <martin.scheibelreiter@chello.at>
2025-04-07 18:27:41 +0200

    .should('have.length', 4) -> .class:visible only in cypress

--- cypress/e2e/examples/Test2.cy.js
+++ cypress/e2e/examples/Test2.cy.js
@@ -11,5 +11,9 @@ describe('Meine Test-Suite', () => {
     it('sollte etwas testen', () => {
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
         cy.get('.search-keyword').type('ca')
+        cy.wait(2000)
+        cy.get('.product').should('have.length', 5)
+        cy.get('.products .product').should('have.length', 4)
+        cy.get('.product:visible').should('have.length', 4) //:visible nur in cypress
     });
 });
