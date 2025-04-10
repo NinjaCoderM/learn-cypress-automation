b603d1cd7e16964e632b27f717dc958c5abb4506
wienm <martin.scheibelreiter@chello.at>
2025-04-10 23:18:33 +0200

    click second button two solutions

--- cypress/e2e/examples/Test2.cy.js
+++ cypress/e2e/examples/Test2.cy.js
@@ -1,21 +1,22 @@
 /// <reference types="cypress" /> --> für TypeScript oder wenn in anderen Ordner cy nicht erkannt wird
-describe('Meine Test-Suite', () => {
-    before(() => {
-        // wird einmal vor allen Tests ausgeführt
-    });
+describe("Meine Test-Suite", () => {
+  before(() => {
+    // wird einmal vor allen Tests ausgeführt
+  });
 
-    beforeEach(() => {
-        // wird vor jedem einzelnen Test ausgeführt
-    });
+  beforeEach(() => {
+    // wird vor jedem einzelnen Test ausgeführt
+  });
 
-    it('sollte etwas testen', () => {
-        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
-        cy.get('.search-keyword').type('ca')
-        cy.wait(2000)
-        cy.get('.product').should('have.length', 5)
-        cy.get('.products .product').should('have.length', 4)
-        cy.get('.product:visible').should('have.length', 4) //:visible nur in cypress
-        cy.get('.products').find('.product').should('have.length', 4)
-        
-    });
+  it("sollte etwas testen", () => {
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    cy.get(".search-keyword").type("ca");
+    cy.wait(2000);
+    cy.get(".product").should("have.length", 5);
+    cy.get(".products .product").should("have.length", 4);
+    cy.get(".product:visible").should("have.length", 4); //:visible nur in cypress
+    cy.get(".products").find(".product").should("have.length", 4);
+    //cy.get('div.products div.product:nth-of-type(2) div button').click()
+    cy.get(".products").find(".product").eq(2).contains("ADD TO CART").click();
+  });
 });
