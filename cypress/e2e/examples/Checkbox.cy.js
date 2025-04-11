441da3e2a694513a8300022453bb8da296b21ea2
wienm <martin.scheibelreiter@chello.at>
2025-04-11 23:17:42 +0200

    Checkbox, .check()

---
+++ cypress/e2e/examples/Checkbox.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" /> --> für TypeScript oder wenn in anderen Ordner cy nicht erkannt wird
+describe("Meine Test-Suite", () => {
+    before(() => {
+      // wird einmal vor allen Tests ausgeführt
+    });
+  
+    beforeEach(() => {
+      // wird vor jedem einzelnen Test ausgeführt
+    });
+  
+    it("sollte etwas testen", () => {
+      cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+      cy.get('#checkBoxOption1').check()
+    });
+});
