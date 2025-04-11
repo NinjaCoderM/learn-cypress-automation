a85838e1e2d833c2116e3218c74cf738dc3af625
wienm <martin.scheibelreiter@chello.at>
2025-04-11 22:03:30 +0200

    alias -> cy.get(".product").as("productLocator")  cy.get("@productLocator").should("have.length", 5);

--- cypress/e2e/examples/Test2.cy.js
+++ cypress/e2e/examples/Test2.cy.js
@@ -12,14 +12,16 @@ describe("Meine Test-Suite", () => {
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
     cy.get(".search-keyword").type("ca");
     cy.wait(2000);
-    cy.get(".product").should("have.length", 5);
+    cy.get(".product").as("productLocator")
+    cy.get("@productLocator").should("have.length", 5);
     cy.get(".products .product").should("have.length", 4);
     cy.get(".product:visible").should("have.length", 4); //:visible nur in cypress
-    cy.get(".products").find(".product").should("have.length", 4);
+    cy.get(".products").find(".product").as("productLocatorInProducts")
+    cy.get("@productLocatorInProducts").should("have.length", 4);
     //cy.get('div.products div.product:nth-of-type(2) div button').click()
     //cy.get(".products").find(".product").eq(2).contains("ADD TO CART").click(); // Besser weil Zwischenergebnisse
     //cy.get(".products").find(".product").contains("Capsicum").parent().contains("ADD TO CART").click();   
-    cy.get(".products").find(".product").each(($el) => {
+    cy.get("@productLocatorInProducts").each(($el) => {
         if ($el.text().includes("Capsicum")) {
           cy.wrap($el).contains("ADD TO CART").click();
         }
@@ -28,6 +30,7 @@ describe("Meine Test-Suite", () => {
       const text = $el.text();
       cy.log(text);
     });
+    
       
   });
 });
