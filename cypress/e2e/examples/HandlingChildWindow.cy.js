9fe9ef73958f04a7283f1d8d83c92ad9467a726a
wienm <martin.scheibelreiter@chello.at>
2025-04-16 20:23:15 +0200

    crossdomain: not allowed to follow link in other domain

--- cypress/e2e/examples/HandlingChildWindow.cy.js
+++ cypress/e2e/examples/HandlingChildWindow.cy.js
@@ -2,7 +2,7 @@ describe("Handling Child Windows", () => {
    it("Should handle child window", () => {
       cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
       cy.get("#opentab").invoke('removeAttr', 'target').click()
-
+      cy.get("#navbarSupportedContent a[href*='about']").click()  //Timed out retrying after 4000ms: The command was expected to run against origin https://rahulshettyacademy.com but the application is at origin https://www.qaclickacademy.com.
     });
 
 
