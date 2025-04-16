69c4f8da50ec986633c2fd175bd749efc3c87e9a
wienm <martin.scheibelreiter@chello.at>
2025-04-16 20:34:41 +0200

    cy.origin should contain

--- cypress/e2e/examples/HandlingChildWindow.cy.js
+++ cypress/e2e/examples/HandlingChildWindow.cy.js
@@ -5,6 +5,7 @@ describe("Handling Child Windows", () => {
        cy.get("#opentab").invoke('removeAttr', 'target').click()
        cy.origin("https://www.qaclickacademy.com", ()=>{
          cy.get("#navbarSupportedContent a[href*='about']").click() 
+         cy.get(".mt-50 h2").should('contain', 'QAClick Academy')
        })
     
      });
