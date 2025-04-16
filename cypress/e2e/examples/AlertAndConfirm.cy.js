00933d3252526bb7b1a514d0ab3cd0878e49c32d
wienm <martin.scheibelreiter@chello.at>
2025-04-16 20:04:06 +0200

    Confirm Event: cy.on('window:confirm', (str) => ...

--- cypress/e2e/examples/AlertAndConfirm.cy.js
+++ cypress/e2e/examples/AlertAndConfirm.cy.js
@@ -5,9 +5,14 @@ describe("Meine Test-Suite", () => {
     cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
     cy.get("#alertbtn").click();
     cy.get("[value='Confirm']").click();
+    cy.on('window:alert', (str) => {
+      //Mocha
+      expect(str).to.equal('Hello , share this practice page and share your knowledge')
+    })
+    cy.on('window:confirm', (str) => {
+      //Mocha
+      expect(str).to.equal('Hello , Are you sure you want to confirm?')
+    })
   });
-  cy.on('window:alert', (str) => {
-    //Mocha
-    expect(str).to.equal('Hello , share this practice page and share your knowledge')
-  })
+
 });
