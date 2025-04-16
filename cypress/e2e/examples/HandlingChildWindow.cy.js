ee6bffac020ef48f1bc460622efc7fbc050641b2
wienm <martin.scheibelreiter@chello.at>
2025-04-16 20:29:42 +0200

    cy.origin for cross domain

--- cypress/e2e/examples/HandlingChildWindow.cy.js
+++ cypress/e2e/examples/HandlingChildWindow.cy.js
@@ -1,9 +1,13 @@
+/// <reference types="cypress" /> --> für TypeScript oder wenn in anderen Ordner cy nicht erkannt wird
 describe("Handling Child Windows", () => {
-   it("Should handle child window", () => {
-      cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
-      cy.get("#opentab").invoke('removeAttr', 'target').click()
-      cy.get("#navbarSupportedContent a[href*='about']").click()  //Timed out retrying after 4000ms: The command was expected to run against origin https://rahulshettyacademy.com but the application is at origin https://www.qaclickacademy.com.
-    });
-
-
-});
+    it("Should handle child window", () => {
+       cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+       cy.get("#opentab").invoke('removeAttr', 'target').click()
+       cy.origin("https://www.qaclickacademy.com", ()=>{
+         cy.get("#navbarSupportedContent a[href*='about']").click() 
+       })
+    
+     });
+ 
+ 
+ });
