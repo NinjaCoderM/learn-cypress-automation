68f9e521440b32f6f6de849a71828602a2080e14
wienm <martin.scheibelreiter@chello.at>
2025-04-16 19:41:18 +0200

    caching Events: cy.on('window:alert', (str) => {expect(str).to.equal('He')...

---
+++ cypress/e2e/examples/AlertAndConfirm.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" /> --> für TypeScript oder wenn in anderen Ordner cy nicht erkannt wird
+describe("Meine Test-Suite", () => {
+
+  it("sollte etwas testen", () => {
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+    cy.get("#alertbtn").click();
+    cy.get("[value='Confirm']").click();
+  });
+  cy.on('window:alert', (str) => {
+    //Mocha
+    expect(str).to.equal('Hello , share this practice page and share your knowledge')
+  })
+});
