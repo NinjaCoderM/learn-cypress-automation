b5b5b865b464c05bb6940c849df310bade55e7e0
wienm <martin.scheibelreiter@chello.at>
2025-04-17 20:04:39 +0200

    Variante eq(index) in: each(($e1, index, $list) => { cy.get('tr td:nth-child(3)').eq(index).should

--- cypress/e2e/examples/HandlingTableAndMouseOver.cy.js
+++ cypress/e2e/examples/HandlingTableAndMouseOver.cy.js
@@ -5,10 +5,10 @@ describe("Handling Table and MouseOver", () => {
        cy.contains('td', 'Master Selenium Automation in simple Python Language').next().should('contain', '25')
 
        cy.get('tr td:nth-child(2)').each(($e1, index, $list) => {
-         if($e1.text().includes('Master Selenium Automation in simple Python Language'))cy.wrap($e1).contains('Master Selenium Automation in simple Python Language').next().should('contain', '25')
+         if($e1.text().includes('Master Selenium Automation in simple Python Language')){
+            cy.wrap($e1).contains('Master Selenium Automation in simple Python Language').next().should('contain', '25')
+            cy.get('tr td:nth-child(3)').eq(index).should('contain.text', '25')
+         }
          })
-    
         });
-    
-    
     });
