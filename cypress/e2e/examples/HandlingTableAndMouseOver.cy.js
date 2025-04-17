289f34bbad22724573b1bd5bff2904b3155f6087
wienm <martin.scheibelreiter@chello.at>
2025-04-17 19:36:09 +0200

    css selector second child 'tr td:nth-child(2)'

--- cypress/e2e/examples/HandlingTableAndMouseOver.cy.js
+++ cypress/e2e/examples/HandlingTableAndMouseOver.cy.js
@@ -4,8 +4,11 @@ describe("Handling Table and MouseOver", () => {
        cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
        cy.contains('td', 'Master Selenium Automation in simple Python Language').next().should('contain', '25')
 
-       
-     });
- 
- 
- });
+       cy.get('tr td:nth-child(2)').each(($e1, index, $list) => {
+         if($e1.text().includes('Master Selenium Automation in simple Python Language'))cy.wrap($e1).contains('Master Selenium Automation in simple Python Language').next().should('contain', '25')
+         })
+    
+        });
+    
+    
+    });
