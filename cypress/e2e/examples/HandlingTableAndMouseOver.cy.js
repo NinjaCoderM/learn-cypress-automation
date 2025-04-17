991b99343ce863e8c5ac4d5e651b94edc87e8df0
wienm <martin.scheibelreiter@chello.at>
2025-04-17 20:17:58 +0200

    Variante .then(($td) => ... trim möglich

--- cypress/e2e/examples/HandlingTableAndMouseOver.cy.js
+++ cypress/e2e/examples/HandlingTableAndMouseOver.cy.js
@@ -7,7 +7,10 @@ describe("Handling Table and MouseOver", () => {
        cy.get('tr td:nth-child(2)').each(($e1, index, $list) => {
          if($e1.text().includes('Master Selenium Automation in simple Python Language')){
             cy.wrap($e1).contains('Master Selenium Automation in simple Python Language').next().should('contain', '25')
-            cy.get('tr td:nth-child(3)').eq(index).should('contain.text', '25')
+            cy.get('tr td:nth-child(3)').eq(index).should('have.text', '25')
+            cy.get('tr td:nth-child(3)').eq(index).then(($td) => {
+              expect($td.text().trim()).to.equal('25');
+            });
          }
          })
         });
