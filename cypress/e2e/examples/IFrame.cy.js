6d7c9118dd41d2b7b17704f87f8d73d035ee5326
wienm <martin.scheibelreiter@chello.at>
2025-04-18 20:23:25 +0200

    npm install -D cypress-iframe

---
+++ cypress/e2e/examples/IFrame.cy.js
@@ -0,0 +1,7 @@
+/// <reference types="cypress" /> --> für TypeScript oder wenn in anderen Ordner cy nicht erkannt wird
+describe("Handling iFrame", () => {
+    it("Test iFrame", () => {
+       //Terminal npm install -D cypress-iframe
+       
+    });
+});
