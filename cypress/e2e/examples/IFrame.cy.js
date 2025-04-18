679ebf11ac3da4506d50d87e1bd4f78174498ae0
wienm <martin.scheibelreiter@chello.at>
2025-04-18 20:53:01 +0200

    Test ifram, frameLoaded cy.iframe().find

--- cypress/e2e/examples/IFrame.cy.js
+++ cypress/e2e/examples/IFrame.cy.js
@@ -1,7 +1,15 @@
-/// <reference types="cypress" /> --> für TypeScript oder wenn in anderen Ordner cy nicht erkannt wird
+/// <reference types="cypress" /> 
+/// <reference types="cypress-iframe" />
+import 'cypress-iframe'
+
 describe("Handling iFrame", () => {
     it("Test iFrame", () => {
        //Terminal npm install -D cypress-iframe
-       
+       cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+       cy.frameLoaded('#courses-iframe')
+       cy.iframe().find("a[href*='mentorship']").eq(0).click()
+       //This pricing title element has 6 inner frames. Currently ,cypress does not give inner frames switching support .
+       //cy.iframe().find("h1[class*='pricing-title']").should('be.visible').should('have.length', 2)
+       cy.iframe().find("a[href*='mentorship']").eq(0).should('have.attr', 'href').and('include', 'mentorship')
     });
 });
