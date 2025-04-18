ee29ce3ee67fbc425e9ed0515dea310293c43c46
wienm <martin.scheibelreiter@chello.at>
2025-04-18 20:02:13 +0200

    open Tab, $e1.prop('href') instead of invoke

--- cypress/e2e/examples/HandlingChildWindow.cy.js
+++ cypress/e2e/examples/HandlingChildWindow.cy.js
@@ -12,12 +12,21 @@ describe("Handling Child Windows", () => {
       cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
       cy.get('#opentab').invoke('prop', 'href').then((href) => {
         cy.visit(href)
-        cy.origin("https://www.qaclickacademy.com", ()=>{
+        cy.origin(href, ()=>{
           cy.get("#navbarSupportedContent a[href*='about']").click() 
           cy.get(".mt-50 h2").should('contain', 'QAClick Academy')
         }) 
       })
     });
-
- 
- });
+    it("Should handle child window, cy.visit instead of remove target $e1.prop", () => {
+      cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+      cy.get('#opentab').then(($e1) => {
+        const url = $e1.prop('href')
+        cy.visit(url)
+        cy.origin(url, ()=>{
+          cy.get("#navbarSupportedContent a[href*='about']").click() 
+          cy.get(".mt-50 h2").should('contain', 'QAClick Academy') 
+        }) 
+      })
+    });
+});
