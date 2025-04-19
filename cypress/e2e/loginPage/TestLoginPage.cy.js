547dcc5a3e347fd8bc8db78a0c3661e0294a3d81
wienm <martin.scheibelreiter@chello.at>
2025-04-19 23:53:11 +0200

    E2E Test, Tip use .type (with Events), .filter(":contains("Blackberry")'), { timeout: 10000 }

---
+++ cypress/e2e/loginPage/TestLoginPage.cy.js
@@ -0,0 +1,60 @@
+/// <reference types="cypress" />
+describe("Test Suite End to End ecommerce Test", ()=>{
+    it("Submit Order", ()=>{
+        cy.visit("https://rahulshettyacademy.com/loginpagePractise/#")
+        cy.get('#username').type("rahulshettyacademy") // invoke geht aber ohne Events .invoke("val", "rahulshettyacademy")
+        cy.get('#password').type("learning")
+        cy.contains("input", "Sign In").click()
+        cy.contains("Shop Name").should("be.visible")
+        cy.get("app-card").should("have.length", 4)
+
+        //cy.contains("button", "Add").eq(1).click() -> geht nicht weil immer nur ein Ergebnis. Add müsste eindeutig sein!
+        cy.contains('Nokia Edge')       
+            .parents('.card')             
+            .find('button.btn-info')      
+            .click();   
+        //cy.get("button:contains('Add')").eq(1).click()
+        cy.get("app-card").filter(":contains('Samsung Note 8')") 
+        .find('button.btn-info')      
+        .click();
+
+        cy.get("app-card").filter(":contains('Blackberry')") 
+        .then($el =>{
+            cy.wrap($el).should("have.length", 1)
+            cy.wrap($el).contains("button", "Add").click()
+        })
+        
+        cy.get("a:contains('Checkout')").click()
+
+        cy.get("tr").each($el => {
+            const $row = Cypress.$($el);
+          
+            const col = $row.find(".col-sm-8");
+            if (col.length > 0 && col.is(":visible")) {
+              const priceText = $row.find("td.col-sm-1 strong").first().text();
+              const value = parseInt(priceText.replace(/[^\d]/g, ''));
+              expect(value).to.be.lessThan(200000);
+            }
+        });
+
+        //alternative vom Vortragenden
+        let sum = 0; 
+        cy.get("tr td:nth-child(4) strong").each($el => {
+            const amount = Number($el.text().split(" ")[1].trim()); 
+            sum = sum + amount
+        }).then(()=>{
+            expect(sum).to.be.lessThan(250000) // muss im then sein, da sonst zu früh getestet, nur cy wird in der richtigen Reihenfolge bearbeitet
+        })
+
+        cy.contains("button", "Checkout").click()
+
+        cy.get("#country").type("Po")
+
+        cy.contains("a", "Poland", { timeout: 10000 }).should("be.visible").click()
+
+        cy.contains("input", "Purchase").click()
+
+        cy.get(".alert-success").should("contain", "Success")
+        
+    })
+})
