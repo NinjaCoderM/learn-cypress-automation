6b8ab712f481f823f12b71764de173c1fbf9c034
wienm <martin.scheibelreiter@chello.at>
2025-04-21 14:10:50 +0200

    cypress custom commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('submitFormDetails', ()=>{
+    cy.get("#country").type("Po")
+    cy.contains("a", "Poland", { timeout: 8000 }).should("be.visible").click()
+    cy.get('input[value="Purchase"]').click()
+})
