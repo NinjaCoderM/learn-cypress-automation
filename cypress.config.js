b2d5b668430a77c6f4f487f29fa1bc68a006d523
wienm <martin.scheibelreiter@chello.at>
2025-04-21 20:02:44 +0200

    cypress-mochawesome-reporter, videos and screenshots

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,38 @@
 const { defineConfig } = require("cypress");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = defineConfig({
   defaultCommandTimeout: 4000,
-  env:{
-    url: "https://rahulshettyacademy.com"
+  env: {
+    url: "https://rahulshettyacademy.com",
+  },
+  reporter: "cypress-mochawesome-reporter",
+  video: true,
+  screenshotsFolder: "cypress/reports/html/screenshots",
+  videosFolder: "cypress/reports/html/videos",
+  reporterOptions: {
+    reportDir: "cypress/reports/html",
+    reportFilename: "index.html",
+    overwrite: false,
+    html: true,
+    json: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    videoOnFailOnly: false,
   },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // 🧹 Automatisch index.html löschen (und optional andere)
+      const reportFolder = path.join(__dirname, "cypress/reports/html");
+      const indexFile = path.join(reportFolder, "index.html");
+
+      // Wenn vorhanden, löschen
+      if (fs.existsSync(indexFile)) {
+        fs.unlinkSync(indexFile);
+        console.log("✅ Alte index.html gelöscht");
+      }
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
     specPattern: "cypress/e2e/**/*.cy.js",
   },
--- cypress/e2e/loginPage/TestLoginPage.cy.js
+++ cypress/e2e/loginPage/TestLoginPage.cy.js
@@ -2,38 +2,39 @@
 
 import LoginPage from "../../support/pageObjects/LoginPage";
 
-describe("Test Suite End to End ecommerce Test", ()=>{
-    let fdata;
-    before(()=>{
-        cy.fixture('example').then((data)=>{
-            fdata = data;
-        })
-    })
+describe("Test Suite End to End ecommerce Test", () => {
+  let fdata;
+  before(() => {
+    cy.fixture("example").then((data) => {
+      fdata = data;
+    });
+  });
 
+  it("Submit Order", () => {
+    Cypress.config("defaultCommandTimeout", 10000);
 
-    it("Submit Order", ()=>{
-        Cypress.config('defaultCommandTimeout', 10000)
+    const loginPage = new LoginPage();
+    loginPage.visit(Cypress.env("url") + "/loginpagePractise/#");
+    cy.screenshot("nach-visit");
+    cy.log(`Username: ${fdata.username}`);
+    const productPage = loginPage.login(fdata.username, fdata.password);
 
-        const loginPage = new LoginPage(); 
-        loginPage.visit(Cypress.env("url") + "/loginpagePractise/#")
-        cy.log(`Username: ${fdata.username}`)
-        const productPage = loginPage.login(fdata.username, fdata.password)
+    productPage.pageValidation();
+    cy.screenshot("ProductPage");
+    productPage.getCardCount().should("have.length", 4);
+    productPage.addCart(fdata.productNameNokia);
+    productPage.addCart(fdata.productNameSamsung);
+    cy.screenshot("Cart-two-products");
+    const cartPage = productPage.goToCart();
 
-        productPage.pageValidation()
-        productPage.getCardCount().should("have.length", 4);
-        productPage.addCart(fdata.productNameNokia)
-        productPage.addCart(fdata.productNameSamsung)
-        const cartPage = productPage.goToCart();
+    cartPage.sumOfProducts().then((sum) => expect(sum).to.be.lessThan(250000));
+    cy.screenshot("cartPage");
+    const confirmationPage = cartPage.checkout();
 
-        cartPage.sumOfProducts().then(sum => expect(sum).to.be.lessThan(250000))
-        
-        const confirmationPage = cartPage.checkout()
+    //cy.pause()
 
-        //cy.pause()
-
-        confirmationPage.submitFormDetails()
-        confirmationPage.getSuccessMessage()    
-        
-        
-    })
-})
+    confirmationPage.submitFormDetails();
+    confirmationPage.getSuccessMessage();
+    cy.screenshot("confirmationPage");
+  });
+});
