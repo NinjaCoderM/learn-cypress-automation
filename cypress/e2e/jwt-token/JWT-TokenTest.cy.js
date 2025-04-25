ce7d28313697714c4793856c638709b89463a4d2
wienm <martin.scheibelreiter@chello.at>
2025-04-25 21:24:01 +0200

    readFile neatCSV

--- cypress/e2e/jwt-token/JWT-TokenTest.cy.js
+++ cypress/e2e/jwt-token/JWT-TokenTest.cy.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
+const neatCSV = require('neat-csv')
 describe('JWT Test Suite', function(){
-    it('JWT Login Test', () => {
+    it('JWT Login Test', async() => {
         cy.LoginAPI().then(()=>{
             cy.visit("https://rahulshettyacademy.com/client", {
                 onBeforeLoad: function(window){
@@ -15,9 +16,18 @@ describe('JWT Test Suite', function(){
         cy.get('button').contains('Cart').click()
         cy.contains('Checkout').click()    
         cy.get('input[placeholder="Select Country"]').type("Ind")
+        cy.wait(1000)
         cy.get('button').contains('India').click()
         cy.contains('Place Order').click()  
         cy.wait(2000)
         cy.get('.order-summary button').contains('CSV').click()
+        
+        const text = cy.readFile(Cypress.config("fileServerFolder") + "/cypress/downloads/order-invoice_test1976.csv")
+        .then(async text=>{
+            const csv = await neatCSV(text)
+            console.log(csv)
+        })
+
+        
     })
 })
